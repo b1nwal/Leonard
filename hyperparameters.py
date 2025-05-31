4aadf37f989e3d921fc46378c3ff562697fdbacc
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-05-31 02:46:23 -0700

    re-segmented main and train because I gave up on tensorboard

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,7 +1,7 @@
 hyperparameters = {
     "learning_rate": 1e-3,
     "batch_size": 32,
-    "train_dataset_size": 15000,
+    "train_dataset_size": 500,
     "corpus_callosum": 0.0,
     "replay_max_probability": 0.8,
     "replay_buffer_length": 1000,
