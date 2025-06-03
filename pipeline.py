7c705b01f4db9af52542218628be3d59144f77d8
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-06-02 23:26:33 -0700

    Switched to precomputed dataset.

--- pipeline.py
+++ pipeline.py
@@ -10,11 +10,6 @@ replay_max_prob = hyperparameters["replay_max_probability"]
 replay_max_len = hyperparameters["replay_buffer_length"]
 train_dataset_size = hyperparameters["train_dataset_size"]
 
-def coord_datagen(): # outputs a batch of coordinates that are approximately uniformly distributed in end effector space.
-    while True:
-        yield tf.random.uniform(minval=-np.pi, maxval=np.pi, shape=(5,))
-    
-coord_dataset = tf.data.Dataset.from_generator(
-    coord_datagen, 
-    output_signature=tf.TensorSpec((5,),dtype="float32")
-).batch(hyperparameters["batch_size"]).prefetch(tf.data.AUTOTUNE)
+data_tensor = tf.random.uniform(minval=-np.pi, maxval=np.pi, shape=(hyperparameters["batch_size"] * hyperparameters["train_dataset_size"],5))
+
+coord_dataset = tf.data.Dataset.from_tensor_slices(data_tensor).batch(hyperparameters["batch_size"]).prefetch(tf.data.AUTOTUNE)
