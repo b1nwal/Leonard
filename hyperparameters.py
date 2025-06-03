c8dca7b2ca9141984d16f0704226050fc915f36b
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-06-03 00:29:04 -0700

    trying some more shit. maybe this will work. forcing gpu on pipeline

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,7 +1,7 @@
 hyperparameters = {
     "learning_rate": 1e-3,
-    "batch_size": 32,
-    "train_dataset_size": 40000,
+    "batch_size": 128,
+    "train_dataset_size": 100,
     "corpus_callosum": 0.0,
     "replay_max_probability": 0.8,
     "replay_buffer_length": 1000,
--- pipeline.py
+++ pipeline.py
@@ -10,6 +10,7 @@ replay_max_prob = hyperparameters["replay_max_probability"]
 replay_max_len = hyperparameters["replay_buffer_length"]
 train_dataset_size = hyperparameters["train_dataset_size"]
 
-data_tensor = tf.random.uniform(minval=-np.pi, maxval=np.pi, shape=(hyperparameters["batch_size"] * hyperparameters["train_dataset_size"],5))
+with tf.device("/GPU:0"):
+    data_tensor = tf.random.uniform(minval=-np.pi, maxval=np.pi, shape=(hyperparameters["batch_size"] * hyperparameters["train_dataset_size"],5))
 
-coord_dataset = tf.data.Dataset.from_tensor_slices(data_tensor).batch(hyperparameters["batch_size"]).prefetch(tf.data.AUTOTUNE)
+    coord_dataset = tf.data.Dataset.from_tensor_slices(data_tensor).batch(hyperparameters["batch_size"]).prefetch(tf.data.AUTOTUNE)
