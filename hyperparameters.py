f564102627be08d75ca97ac487c4f9715e0de9d7
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-06-04 00:41:27 -0700

    fine tuning

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,12 +1,12 @@
 hyperparameters = {
     "learning_rate": 1e-3,
     "batch_size": 128,
-    "train_dataset_size": 200000,
+    "train_dataset_size": 250000,
     "corpus_callosum": 0.0,
     "replay_max_probability": 0.8,
     "replay_buffer_length": 1000,
     "jitter_multiplier": 0.01,
-    "motion_penalty_multiplier": 0.02,
+    "motion_penalty_multiplier": 0.025,
 }
 
 armparameters = {
