39388979d75f4bcef171d59cc2bef06426e23aad
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-06-04 00:11:01 -0700

    added distance metric.

--- main.py
+++ main.py
@@ -99,11 +99,12 @@ class Leonard(tf.keras.Model):
             fx = fwd(x)
             y = self.call(tf.concat((fx,rs),axis=1),training=True)
             rotsum = tf.reduce_sum(abs(rs - y),axis=1)
-            loss = eudist(fwd(y),fx) + (hyperparameters["motion_penalty_multiplier"] * (f / hyperparameters["train_dataset_size"])) * rotsum
+            dist = eudist(fwd(y),fx)
+            loss = dist + (hyperparameters["motion_penalty_multiplier"] * (f / hyperparameters["train_dataset_size"])) * rotsum
         gradient = tape.gradient(loss, self.trainable_variables)
         self.grads(gradient)
         # mmax = tf.reduce_max(loss)
-        return (tf.reduce_mean(loss), tf.reduce_mean(rotsum))
+        return (dist, tf.reduce_mean(loss), tf.reduce_mean(rotsum))
 
 
 leo = Leonard()
@@ -113,9 +114,9 @@ np.set_printoptions(threshold=1000, linewidth=200)   # set wide line
 
 session = base64.b64encode(datetime.datetime.now().ctime().encode('utf-8')).decode('utf-8')
 for f,x in enumerate(dataset,1):
-    mloss, rd = leo.train_step(tf.constant([f],dtype="float32"), x[:,:5],x[:,5:])
+    dist, mloss, rd = leo.train_step(tf.constant([f],dtype="float32"), x[:,:5],x[:,5:])
     percentage = int((f/hyperparameters["train_dataset_size"])*20)
-    print("\r["+"="*percentage + ">" + " "*(20-percentage) + "]","Sample: {f}/{h}, Loss: {m:.2f}, Rotation Delta: {rd:.2f}".format(rd=rd,f=f,m=mloss,h=hyperparameters["train_dataset_size"]),end="")
+    print("\r["+"="*percentage + ">" + " "*(20-percentage) + "]","Sample: {f}/{h}, Loss: {m:.2f}, Rotation Delta: {rd:.2f}, Dist: {dist:.2f}".format(dist=dist,rd=rd,f=f,m=mloss,h=hyperparameters["train_dataset_size"]),end="")
 
     if f%1000==0:
         leo.save(("leo_v1-2-4/" + session + ".keras"))
