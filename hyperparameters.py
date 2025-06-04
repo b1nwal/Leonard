aea4820face8593baec4aa4181655d7dbcafd662
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2025-06-03 22:51:52 -0700

    training param adjust

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,7 +1,7 @@
 hyperparameters = {
     "learning_rate": 1e-3,
-    "batch_size": 128,
-    "train_dataset_size": 40000,
+    "batch_size": 256,
+    "train_dataset_size": 200000,
     "corpus_callosum": 0.0,
     "replay_max_probability": 0.8,
     "replay_buffer_length": 1000,
